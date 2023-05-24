fa68c48a1682de7cbb915bf53c7fdeabfa15c564
Chigozie Ike <mrchigozieike@gmail.com>
2023-05-24 12:47:15 +0100

    all time fixes

---
+++ lesson_2/scripts/scripts.js
@@ -0,0 +1,7 @@
+const d = new Date();
+
+  document.querySelector("#currentyear").textContent = d.getFullYear();
+
+
+  let oLastModif = new Date();
+  document.querySelector("#lastmodified").textContent = oLastModif;
