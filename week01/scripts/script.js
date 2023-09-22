e1fe47b6f622a0b926cb3ba88017af6063f1d2c9
chigozie <mrchigozieike@gmail.com>
2023-09-22 11:27:51 +0100

    week02 update

---
+++ week01/scripts/script.js
@@ -0,0 +1,17 @@
+// Get the current date
+const currentDate = new Date();
+
+// Extract individual components (year, month, day, etc.) if needed
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1; // Months are zero-indexed, so we add 1 to get the correct month (January is 0, February is 1, and so on)
+const day = currentDate.getDate();
+
+// Display the date in a specific format (e.g., YYYY-MM-DD)
+const formattedDate = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+
+
+document.querySelector("#year").textContent = year;
+
+document.querySelector(".month").textContent = month;
+document.querySelector(".day").textContent = day;
+document.querySelector("#currentyear").textContent = year;
