ea199db2bb3d3effd20aa4a9fbaa5accdb22ac11
chigozie <mrchigozieike@gmail.com>
2023-10-20 11:02:17 +0100

    final week 6

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,19 +1,16 @@
-// Get the current temperature and wind speed from the HTML
-const currentTemperature = parseFloat(document.querySelector('.temperature').textContent);
-const windSpeed = parseFloat(document.querySelector('.windspeed').textContent);
+// Get the temperature and wind speed values from the HTML document
+let temperature = parseFloat(document.getElementById('temperature').textContent);
+let windSpeed = parseFloat(document.getElementById('windSpeed').textContent);
 
-// Function to calculate wind chill
-function calculateWindChill(temperature, speed) {
-  if (temperature <= 50 && speed > 3.0) {
-    const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temperature * Math.pow(speed, 0.16);
-    return Math.round(windChill * 10) / 10; // Round to one decimal place
-  } else {
-    return 'N/A';
-  }
+// Check if the values meet the specification limits
+if (temperature <= 50 && windSpeed > 3.0) {
+    // Calculate the wind chill factor
+    let windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+    // Round the result to a whole number
+    windChill = Math.round(windChill);
+    // Display the wind chill factor value
+    document.getElementById('windChill').textContent = windChill;
+} else {
+    // Display "N/A" if the input values did not meet the requirements
+    document.getElementById('windChill').textContent = 'N/A';
 }
-
-// Calculate wind chill
-const windChillValue = calculateWindChill(currentTemperature, windSpeed);
-
-// Update the HTML with the wind chill value
-document.querySelector('.windchill').textContent = `Wind Chill: ${windChillValue}°F`;
