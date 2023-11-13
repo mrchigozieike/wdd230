9db247783db35317676f36f2985734ed8fa3b1ef
chigozie <mrchigozieike@gmail.com>
2023-11-13 12:29:42 +0100

    weather

--- week10/scripts/weather.js
+++ week10/scripts/weather.js
@@ -5,26 +5,32 @@ const captionDesc = document.querySelector('figcaption');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=15.76947&lon=-86.79603&appid=8f775f4f608fe18d0e030e9dd8c7c309';
 
-async function apiFetch(url) {
-    try {
+
+async function apiFetch() {
+  try {
       const response = await fetch(url);
       if (response.ok) {
-        const data = await response.json();
-        //console.log(data); // testing only
-        displayResults(data); // uncomment when ready
+          const data = await response.json();
+          //console.log(data); // this is for testing the call
+          displayResults(data);
       } else {
           throw Error(await response.text());
       }
-    } catch (error) {
-        console.log(error);
-    }
+  } catch (error) {
+      console.log(error);
   }
-  function displayResults(data) {
-    currentTemp.innerHTML = `~  ${data.current.temp}&deg;C`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`;
-    let desc = data.current.weather[0].description;
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', 'weather icon');
-    captionDesc.textContent = `${desc} ~  `;
-  }
-  apiFetch(url);
+}
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+}
+apiFetch();
